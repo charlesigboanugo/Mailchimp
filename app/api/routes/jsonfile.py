c0bd1b371406787b050f9500f9af55a37cebad36
charles <charlesigboanugo033@gmail.com>
2025-02-22 20:20:07 +0100

    add app logoA

--- app/api/routes/jsonfile.py
+++ app/api/routes/jsonfile.py
@@ -75,7 +75,7 @@ def getJson():
             "app_name": "stackerX",
             "app_description": "good and great",
             "app_logo": "http://ec2-16-171-113-126.eu-north-1.compute.amazonaws.com/static/favicon.ico",
-            "app_url": "\"http://ec2-16-171-113-126.eu-north-1.compute.amazonaws.com",
+            "app_url": "http://ec2-16-171-113-126.eu-north-1.compute.amazonaws.com",
             "background_color": "#fff"
             },
             "is_active": True,
