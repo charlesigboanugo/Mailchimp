7f33b3da85a6fdf5bfd1999be9313bc1ecab0336
charles <charlesigboanugo033@gmail.com>
2025-02-22 16:48:38 +0100

    add app logo

--- app/api/router.py
+++ app/api/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
+from fastapi.staticfiles import StaticFiles
 
 from app.api.routes import jsonfile
 
 api_router = APIRouter()
+api_router.mount("/static", StaticFiles(directory="static"), name="static")
 
 api_router.include_router(jsonfile.router, prefix="/json", tags=["jsonD"])
 
