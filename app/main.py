de66449f4d75a053c0b34578372c513bd00f7b3d
charles <charlesigboanugo033@gmail.com>
2025-02-22 16:48:38 +0100

    add app logo

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
@@ -16,6 +17,8 @@ app.add_middleware(CORSMiddleware,
                    allow_headers=["*"])
 
 app.include_router(api_router, prefix=settings.API_PREFIX, tags=["api"])
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get('/')
 async def home():
