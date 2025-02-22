f6a3052834ba43d9304e48f1f01a164ef94057eb
charles <charlesigboanugo033@gmail.com>
2025-02-22 21:38:10 +0100

    mend

--- app/api/routes/jsonfile.py
+++ app/api/routes/jsonfile.py
@@ -64,7 +64,7 @@ def getJson():
                     ],
                     "target_url": f'{settings.BASE_URL}{settings.API_PREFIX}'
                 }
-            }"""
+            }
   jsonData = {
         "data": {
             "date": {
@@ -107,6 +107,50 @@ def getJson():
             ],
             "target_url": "http://ec2-16-171-113-126.eu-north-1.compute.amazonaws.com/api/v1",
             "tick_url": "http://ec2-16-171-113-126.eu-north-1.compute.amazonaws.com/api/v1"
+        }
+    }"""
+  jsonData = {
+        "data": {
+            "date": {
+            "created_at": "2025-02-06",
+            "updated_at": "2025-01-28"
+            },
+            "descriptions": {
+            "app_name": "stackerX",
+            "app_description": "good and great",
+            "app_logo": "https://mailchimp-hqu1.onrender.com/static/stackerx.png",
+            "app_url": "https://mailchimp-hqu1.onrender.com/",
+            "background_color": "#fff"
+            },
+            "is_active": True,
+            "integration_type": "modifier",
+            "integration_category": "Marketing Automation",
+            "key_features": [
+            "great easy"
+            ],
+            "author": "CHARLES",
+            "settings": [
+            {
+                "label": "password",
+                "type": "text",
+                "required": True,
+                "default": "password"
+            },
+            {
+                "label": "name",
+                "type": "text",
+                "required": True,
+                "default": "gabriel"
+            },
+            {
+                "label": "second name",
+                "type": "text",
+                "required": True,
+                "default": "james"
+            }
+            ],
+            "target_url": "https://mailchimp-hqu1.onrender.com/api/v1",
+            "tick_url": "https://mailchimp-hqu1.onrender.com/api/v1"
         }
     }
   return jsonData
